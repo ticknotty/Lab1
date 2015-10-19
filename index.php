90ae17650b50aaf9e8b18f14651e4823054e5d0b
ticknotty <nicktotty8@gmail.com>
2015-10-19 14:52:07 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello, world";
+    echo "Hello," . " " . "world" . "!";
+    echo 5 * 7;
+    ?>
+</p>
+</body>
+</html>

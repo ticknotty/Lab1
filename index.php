360274b5a135237a6b6ce58295c9b01b75638fde
ticknotty <nicktotty8@gmail.com>
2015-10-19 15:03:35 +0100

    Added Frodo

--- index.php
+++ index.php
@@ -5,10 +5,15 @@
 <body>
 <p>
     <?php
-    echo "\nHello, world ";
-    echo "\nHello," . " " . "world" . "!";
-    echo 5 * 7;
+//    echo "Hello, world ";
+//    echo "Hello," . " " . "world" . "!";
+//    echo 5 * 7;
+    $myname = "Frodo Baggins";
+    $myage = 111;
+    echo "My name is" . $myname . "and I am " . $myage;
     ?>
+
+
 </p>
 </body>
 </html>

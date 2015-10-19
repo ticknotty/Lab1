aeafce71b1b3335bbebe590c2b602cc6c717b8e0
ticknotty <nicktotty8@gmail.com>
2015-10-19 15:56:45 +0100

    Added an array Modified an array in position 1

--- index.php
+++ index.php
@@ -70,11 +70,11 @@
 
     $myArray = array("do", "re", "mi"); //declares the array
 
-    echo $myArray[0] // outputs "do"
+    echo $myArray[0]; // outputs "do"
 
-//    $myArray[1] = "la"; // modifies position 1 (re)
+    $myArray[1] = "la"; // modifies position 1 (re)
 
-//    echo $myArray[1] // outputs "la"
+    echo $myArray[1]; // outputs "la"
 
 //    unset($array[2]); //removes the array in position 2 "mi"
     ?>

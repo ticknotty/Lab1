2aac98872cf936763a58d98d747b00bddfbdcfd5
ticknotty <nicktotty8@gmail.com>
2015-10-19 15:45:16 +0100

    Added hobbits

--- index.php
+++ index.php
@@ -19,22 +19,38 @@
 //    else {
 //        print "Who are you?";
 //    }
-    $name = "Nick";
-    $age = 15;
+//    $name = "Nick";
+//    $age = 15;
 
-    if ($age > 21){
-        print $name . ", you can buy specs, mugs AND sausage rolls.";
-    }
+//    if ($age > 21){
+//        print $name . ", you can buy specs, mugs AND sausage rolls.";
+//    }
 
-    else if ($age >= 18 && $age < 21){
-        print $name . ", you can buy specs and mugs.";
-    }
+//    else if ($age >= 18 && $age < 21){
+//        print $name . ", you can buy specs and mugs.";
+//    }
 
-    else if ($age >= 16 && $age < 18){
-        print $name . ", you can only buy specs.";
-    }
-    else {
-        print $name . ", you are not allowed to buy specs, mugs or SAUSAGE ROLLS";
+//    else if ($age >= 16 && $age < 18){
+//        print $name . ", you can only buy specs.";
+//    }
+//    else {
+//        print $name . ", you are not allowed to buy specs, mugs or SAUSAGE ROLLS.";
+//    }
+
+    $numberOfHobbits = 2;
+
+    switch ($numberOfHobbits){
+        case 1:
+            echo "1 sad hobbit";
+            break;
+        case 2:
+            echo "2 happy hobbits";
+            break;
+        case 3:
+            echo "3 hobbits are a crowd";
+            break;
+        default:
+            echo "All the hobbits have gone home";
     }
     ?>
 
